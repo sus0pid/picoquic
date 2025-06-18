73607407c93a620231ab5aaf487b8459ed7b8f90
sus0pid <s2136500@ed.ac.uk>
2025-06-18 20:13:49 +0100

    test with picotls libs

--- cmake/FindPTLS.cmake
+++ cmake/FindPTLS.cmake
@@ -1,5 +1,7 @@
 # - Try to find Picotls in hsig_picotls dir @maxin
 
+set(HSIG_PICOTLS_DIR ../picotls)
+
 if (PICOQUIC_FETCH_PTLS)
     set(PTLS_CORE_LIBRARY picotls-core)
     set(PTLS_MINICRYPTO_LIBRARY picotls-minicrypto)
@@ -23,22 +25,27 @@ if (PICOQUIC_FETCH_PTLS)
             set(PTLS_LIBRARIES ${PTLS_CORE_LIBRARY} ${PTLS_OPENSSL_LIBRARY} ${PTLS_FUSION_LIBRARY} ${PTLS_MINICRYPTO_LIBRARY})
         else()
             set(PTLS_WITH_FUSION_DEFAULT OFF)
-            set(PTLS_LIBRARIES ${PTLS_CORE_LIBRARY} ${PTLS_OPENSSL_LIBRARY}  ${PTLS_MINICRYPTO_LIBRARY})
+            set(PTLS_LIBRARIES ${PTLS_CORE_LIBRARY} ${PTLS_OPENSSL_LIBRARY} ${PTLS_MINICRYPTO_LIBRARY})
             unset(PTLS_FUSION_LIBRARY)
         endif()
     endif()
-    set(PTLS_INCLUDE_DIRS ${picotls_SOURCE_DIR}/include)
+    set(PTLS_INCLUDE_DIRS ${HSIG_PICOTLS_DIR}/include)
 else(PICOQUIC_FETCH_PTLS)
-    # old: ${CMAKE_SOURCE_DIR}/../picotls/include
-    # old: ${CMAKE_BINARY_DIR}/../picotls/include
     find_path(PTLS_INCLUDE_DIR
         NAMES picotls/openssl.h
-        HINTS ${PTLS_PREFIX}/include/picotls
-            ${CMAKE_SOURCE_DIR}/../hsig_picotls/include 
-            ${CMAKE_BINARY_DIR}/../hsig_picotls/include 
-            ../hsig_picotls/include/ ) 
+        HINTS
+            ${PTLS_PREFIX}/include/picotls
+            ${HSIG_PICOTLS_DIR}/include
+            ${CMAKE_SOURCE_DIR}/${HSIG_PICOTLS_DIR}/include
+            ${CMAKE_BINARY_DIR}/${HSIG_PICOTLS_DIR}/include
+    )
 
-    set(PTLS_HINTS ${PTLS_PREFIX}/lib ${CMAKE_BINARY_DIR}/../hsig_picotls ../hsig_picotls)
+    set(PTLS_HINTS
+        ${PTLS_PREFIX}/lib
+        ${HSIG_PICOTLS_DIR}
+        ${CMAKE_BINARY_DIR}/${HSIG_PICOTLS_DIR}
+        ${CMAKE_SOURCE_DIR}/${HSIG_PICOTLS_DIR}
+    )
 
     find_library(PTLS_CORE_LIBRARY picotls-core HINTS ${PTLS_HINTS})
     find_library(PTLS_MINICRYPTO_LIBRARY picotls-minicrypto HINTS ${PTLS_HINTS})
@@ -60,9 +67,6 @@ else(PICOQUIC_FETCH_PTLS)
 
         if(NOT PTLS_FUSION_LIBRARY)
             include(FindPackageHandleStandardArgs)
-            # handle the QUIETLY and REQUIRED arguments and set PTLS_FOUND to TRUE
-            # if all listed variables are TRUE
-
             find_package_handle_standard_args(PTLS REQUIRED_VARS
                 PTLS_CORE_LIBRARY
                 PTLS_OPENSSL_LIBRARY
@@ -76,8 +80,6 @@ else(PICOQUIC_FETCH_PTLS)
             endif()
         else()
             include(FindPackageHandleStandardArgs)
-            # handle the QUIETLY and REQUIRED arguments and set PTLS_FOUND to TRUE
-            # if all listed variables are TRUE
             find_package_handle_standard_args(PTLS REQUIRED_VARS
                 PTLS_CORE_LIBRARY
                 PTLS_OPENSSL_LIBRARY
