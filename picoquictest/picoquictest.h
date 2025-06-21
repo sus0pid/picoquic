257eee198f27a7805c5bdf04d8640cc10efd175f
sus0pid <s2136500@ed.ac.uk>
2025-06-21 13:10:53 +0100

    declare tls_api_connect_test

--- picoquictest/picoquictest.h
+++ picoquictest/picoquictest.h
@@ -61,7 +61,7 @@ int sendacktest();
 int sendack_loop_test();
 int ackfrq_basic_test();
 int ackfrq_short_test();
-#if 0
+#if 1
 /* The TLS API connect test is only useful when debugging issues step by step */
 int tls_api_connect_test();
 #endif
